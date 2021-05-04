8a76213dc6210de19392466e7bc14d75bbdf2f87
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-05-04 16:44:23 +0900

    add: related user apis

---
+++ react-and-go/go-admin/database/connect.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"github.com/tg112/go/go-admin/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	database, err := gorm.Open(mysql.Open("root@/react_go?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	DB = database
+
+	database.AutoMigrate(&models.User{})
+}
