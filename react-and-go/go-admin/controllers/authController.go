03f3aa2ca4392653a134ea15c7bd01c965df093f
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-05-04 18:35:10 +0900

    add: middleware of auth

--- react-and-go/go-admin/controllers/authController.go
+++ react-and-go/go-admin/controllers/authController.go
@@ -4,10 +4,10 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"github.com/tg112/go/go-admin/database"
 	"github.com/tg112/go/go-admin/models"
+	jwt "github.com/tg112/go/go-admin/util"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -63,12 +63,7 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer:    strconv.Itoa(int(user.Id)),
-		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	token, err := claims.SignedString([]byte("secret"))
+	token, err := jwt.GenerateJwt(strconv.Itoa(int(user.Id)))
 
 	if err != nil {
 		return c.SendStatus(fiber.StatusInternalServerError)
@@ -88,27 +83,13 @@ func Login(c *fiber.Ctx) error {
 	})
 }
 
-type Claims struct {
-	jwt.StandardClaims
-}
-
 func User(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 
-	token, err := jwt.ParseWithClaims(cookie, &Claims{}, func(token *jwt.Token) (interface{}, error) {
-		return []byte("secret"), nil
-	})
-
-	if err != nil || !token.Valid {
-		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"message": "unauthenticated",
-		})
-	}
-	claims := token.Claims.(*Claims)
+	id, _ := jwt.ParseJwt(cookie)
 
 	var user models.User
-	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	database.DB.Where("id = ?", id).First(&user)
 
 	return c.JSON(user)
 }
--- react-and-go/go-admin/routes/routes.go
+++ react-and-go/go-admin/routes/routes.go
@@ -3,11 +3,15 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/tg112/go/go-admin/controllers"
+	"github.com/tg112/go/go-admin/middleware"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+
+	app.Use(middleware.IsAuthenticated)
+
 	app.Post("/api/logout", controllers.Logout)
 	app.Get("/api/user", controllers.User)
 }
